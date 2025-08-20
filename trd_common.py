48ec693fe96f5a7f46e61abfafe7b47efb98f5ef
Cz1975 <75206117+Cz1975@users.noreply.github.com>
2025-08-20 13:43:47 +0200

    Update trd_common.py
    TO
    WTF

--- trd_common.py
+++ trd_common.py
@@ -14,7 +14,7 @@ from solders.keypair import Keypair
 from solders.pubkey import Pubkey
 from solana.rpc.async_api import AsyncClient
 from solana.rpc.commitment import Confirmed
-from solana.transaction import VersionedTransaction
+from solana.transaction_versioned import VersionedTransaction
 
 from jup_python_sdk.clients.ultra_api_client import UltraApiClient
 from jup_python_sdk.models.ultra_api.ultra_order_request_model import UltraOrderRequest
@@ -444,6 +444,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
